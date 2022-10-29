72e24fa6ac6d2c8e66bb14a45c53c8859f797438
Frendlyindy <frendlyindian@gmail.com>
2022-10-28 20:43:29 -0600

    discovery

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -41,7 +41,20 @@ const today = new Date();
   let windspeed = 10;
   setwindchill(temp, windspeed)
 
-  
+  let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) =>{
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () =>{
+        image.removeAttribute("data-src");
+    };
+};
+
+imagesToLoad.forEach((res) => {
+    loadImages(res);
+});
+
+
 
   
 
