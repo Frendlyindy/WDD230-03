d75fd3cc15fdc741af00f2dec6dc8d112dd46f05
Frendlyindy <frendlyindian@gmail.com>
2022-11-19 17:37:25 -0700

    gotahm weather

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -21,25 +21,39 @@ const today = new Date();
     document.querySelector("header p").style.display='none';
   }
 
-  function setwindchill(temp, windspeed){
-    let tempobj = document.querySelector("#temp");
-    let windspeedobj = document.querySelector("#speed");
-    let windchillobj = document.querySelector("#chill");
-    let windchillmeg = "N/A";
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=41.8781&lon=87.6298&appid=7b0ee1b9b07eebcc18188c3f0c258a2b&units=imperial"
 
-  if (temp <= 50 && windspeed > 3){
-    let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
-    windchillmeg = `${chill}&deg; F`;
-  };
-  
-  tempobj.textContent = temp;
-  windspeedobj.textContent = windspeed;
-  windchillobj.innerHTML = windchillmeg;
+  function showWeather(obj){
+     
+      let weatherIcon = document.querySelector("#weathericon");
+      let figureCaption = document.querySelector("figcaption");
+      let tempobj = document.querySelector("#temp");
+      let windspeedobj = document.querySelector("#speed");
+      let windchillobj = document.querySelector("#chill");
+      let windchillmeg = "N/A";
+      let icon = `http://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`;
+      let temp = obj.main.temp;
+      let windspeed = obj.wind.speed;
+      if (temp <= 50 && windspeed > 3){
+        let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+        windchillmeg = `${chill}&deg; F`;
+      };
+      
+      tempobj.textContent = temp + '°F';
+    windspeedobj.textContent = 'Windspeed = ' + windspeed;
+    windchillobj.innerHTML = 'Windchill = ' + windchillmeg;
+      // iconPath.textContent = icon;
+      weatherIcon.setAttribute("src", icon);
+      weatherIcon.setAttribute("alt", obj.weather[0].description);
+      figureCaption.textContent = obj.weather[0].main;
   }
-
-  let temp = 33;
-  let windspeed = 10;
-  setwindchill(temp, windspeed)
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((jsObject) => {
+      console.log(jsObject);
+      showWeather(jsObject)
+    });
+  
 
   let imagesToLoad = document.querySelectorAll("img[data-src]");
 
--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -5,8 +5,21 @@ function showWeather(obj){
     let iconPath = document.querySelector("#icon-src");
     let weatherIcon = document.querySelector("#weathericon");
     let figureCaption = document.querySelector("figcaption");
+    let tempobj = document.querySelector("#temp");
+    let windspeedobj = document.querySelector("#speed");
+    let windchillobj = document.querySelector("#chill");
+    let windchillmeg = "N/A";
     let icon = `http://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`;
+    let temp = obj.main.temp;
+    let windspeed = obj.wind.speed;
+    if (temp <= 50 && windspeed > 3){
+      let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+      windchillmeg = `${chill}&deg; F`;
+    };
     currenttemp.textContent = obj.main.temp;
+    tempobj.textContent = temp;
+  windspeedobj.textContent = windspeed;
+  windchillobj.innerHTML = windchillmeg;
     iconPath.textContent = icon;
     weatherIcon.setAttribute("src", icon);
     weatherIcon.setAttribute("alt", obj.weather[0].description);
